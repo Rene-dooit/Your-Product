41ef5be3a984e4fe8300a62ac962a5d696a2817f
Rene van Reekum <rene@dooit>
2021-09-16 15:59:46 +0000

    My first module

--- my_module/controllers/controllers.py
+++ my_module/controllers/controllers.py
@@ -2,7 +2,7 @@
 from odoo import http
 
 
- class MyModule(http.Controller):
+class MyModule(http.Controller):
      @http.route('/my_module/my_module/', auth='public')
      def index(self, **kw):
          return "Hello, world"
